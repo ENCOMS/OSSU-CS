8d8cd16f1c76da446ad3ea8a29716bdcdfb1e2ae
ENCOMS <garciaricardojf@gmail.com>
2024-10-15 23:07:49 -0400

    feat(update) finally completed the tests as was requested in the requirements for the test_fuel.py file

--- intro-CS/introduction-to-programming/CS50P-Introduction-To-Programming-With-Python/course-ex/ProblemSet_5/test_fuel/test_fuel.py
+++ intro-CS/introduction-to-programming/CS50P-Introduction-To-Programming-With-Python/course-ex/ProblemSet_5/test_fuel/test_fuel.py
@@ -11,16 +11,24 @@ def test_not_characters_variables2():
     with pytest.raises(ValueError):
         convert("1/dog")
 
+
 def test_not_characters_variables3():
     with pytest.raises(ValueError):
         convert("3/1")
+        
 
 def test_not_divided_by_0():
     with pytest.raises(ZeroDivisionError):
         convert("1/0")
 
 
+def test_convert():
+	assert convert("1/2") == 50
+	assert convert("3/4") == 75
+
+
 def test_gauge():
+	assert gauge(0) == "E"
     assert gauge(1) == "E"
     assert gauge(10) == "10%"
     assert gauge(101) == "F"
